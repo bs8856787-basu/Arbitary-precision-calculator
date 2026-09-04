0550bc612c836c563f6a6f8818c0b0661ade6010
bs8856787-cloud <bs8856787@gmail.com>
2026-09-04 10:28:12 +0000

    add APC code

---
+++ apc.h
@@ -0,0 +1,20 @@
+#ifndef APC_H
+#define APC_H
+
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+Node* createNode(int data);
+void insertLast(Node **head, Node **tail, int data);
+void printList(Node *head);
+Node* reverseList(Node *head);
+
+Node* addLists(Node *list1, Node *list2);
+
+Node* subtractLists(Node *list1, Node *list2);
+Node* multiplyLists(Node *list1, Node *list2);
+Node* divideLists(Node *list1, Node *list2);
+
+#endif
